a18e5661b59ce7632436a9389ff69bfb2eeb5b48
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-23 01:48:23 -0500

    added functionality to update blog postings, only if you are the author

--- backend/user_account/urls.py
+++ backend/user_account/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path("register_user/", views.register_user, name="register_user"),  
     path("create_blog/", views.create_blog, name="create_blog"), 
     path("blog_list/", views.blog_list, name="blog_list"),
+    path("update_blog/<int:pk>/", views.update_blog, name="update_blog"),
 ]
 
--- backend/user_account/views.py
+++ backend/user_account/views.py
@@ -33,3 +33,19 @@ def blog_list(request):
 	blogs = Blog.objects.all()
 	serializer = BlogSerializer(blogs, many=True)
 	return Response(serializer.data)
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def update_blog(request, pk):
+	user = request.user
+	blog = Blog.objects.get(id=pk)
+
+	if blog.author != user:
+		return Response({"error": "You are not the author of this post!"}, status=status.HTTP_403_FORBIDDEN)
+	
+	serializer = BlogSerializer(blog, data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+		return Response(serializer.data)
+	
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
