610e2f6f2aba0d4aa793550a64ca041af221103f
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-01-23 14:43:59 -0500

    updated blog-list view to include pagination

--- backend/user_account/views.py
+++ backend/user_account/views.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.pagination import PageNumberPagination
+
+class BlogListPagination(PageNumberPagination):
+	page_size = 3
 
 @api_view(["POST"])
 def register_user(request):
@@ -43,8 +47,11 @@ def create_blog(request):
 @api_view(["GET"])
 def blog_list(request):
 	blogs = Blog.objects.all()
-	serializer = BlogSerializer(blogs, many=True)
-	return Response(serializer.data)
+	paginator = BlogListPagination()
+	paginated_blogs = paginator.paginate_queryset(blogs, request)
+
+	serializer = BlogSerializer(paginated_blogs, many=True)
+	return paginator.get_paginated_response(serializer.data)
 
 @api_view(["PUT"])
 @permission_classes([IsAuthenticated])
