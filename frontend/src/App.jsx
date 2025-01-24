76e0aa93cc1626d9e89264f63671a69d442c8430
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-23 21:40:38 -0500

    imported shadcn button component and added in for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import "./App.css";
+import { Button } from "@/components/ui/button";
+import "./index.css";
 
-const App = () => {
-  return <div>Hello World</div>;
-};
-
-export default App;
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-blue-800">Click me</Button>
+    </div>
+  );
+}
