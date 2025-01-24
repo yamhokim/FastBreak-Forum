d1aaea104b62e0bc740ee5a7a78c40ae019bda14
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-23 22:48:11 -0500

    implemented responsive navbar component and updated navbar component to account for this change

--- frontend/src/ui_components/NavBar.jsx
+++ frontend/src/ui_components/NavBar.jsx
@@ -1,25 +1,35 @@
-import React from "react";
 import { Switch } from "@/components/ui/switch";
 import { FaHamburger } from "react-icons/fa";
+import ResponsiveNavBar from "./ResponsiveNavBar";
+import { useState } from "react";
 
 const NavBar = () => {
-  return (
-    <nav>
-      <nav className="max-container padding-x py-6 flex justify-between items-center gap-6 sticky top-0 z-10 bg-[#FFFFFF] dark:bg-[#141624]"></nav>
-      <a to="/" className="text-[#141624] text-2xl dark:text-[#FFFFFF]">
-        FastBreak Forum
-      </a>
-      <ul className="flex items-center justify-end gap-9 text-[#3B3C4A] lg:flex-1 max-md:hidden dakr:text-[#FFFFFF]">
-        <li>Hi, Clinton</li>
-        <li>Logout</li>
-        <li>Login</li>
-        <li>Register</li>
-        <li className="font-semibold">Create Post</li>
-      </ul>
+  const [showNavBar, setShowNavBar] = useState(false);
 
-      <Switch />
-      <FaHamburger className="text-2xl cursor-pointer hidden max-md:block dark:text-white" />
-    </nav>
+  return (
+    <>
+      <nav className="max-container padding-x py-6 flex justify-between items-center  gap-6 sticky top-0 z-10 bg-[#FFFFFF] dark:bg-[#141624]">
+        <a to="/" className="text-[#141624] text-2xl dark:text-[#FFFFFF]">
+          DevFolio
+        </a>
+        <ul className="flex items-center  justify-end gap-9 text-[#3B3C4A] lg:flex-1 max-md:hidden dark:text-[#FFFFFF]">
+          <li>Hi, Clinton</li>
+
+          <li>Logout</li>
+          <li>Login</li>
+          <li>Register</li>
+          <li className="font-semibold">Create post</li>
+        </ul>
+
+        <Switch />
+        <FaHamburger
+          className="text-2xl cursor-pointer hidden max-md:block dark:text-white"
+          onClick={() => setShowNavBar((curr) => !curr)}
+        />
+      </nav>
+
+      {showNavBar && <ResponsiveNavBar />}
+    </>
   );
 };
 
