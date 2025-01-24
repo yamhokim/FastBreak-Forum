c2d78cde3006ae76492831de147f8dc027036a37
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-23 22:25:18 -0500

    added switch component from shadcn/ui

---
+++ frontend/src/ui_components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Switch } from "@/components/ui/switch";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <nav className="max-container padding-x py-6 flex justify-between items-center gap-6 sticky top-0 z-10 bg-[#FFFFFF] dark:bg-[#141624]"></nav>
+      <a to="/" className="text-[#141624] text-2xl dark:text-[#FFFFFF]">
+        FastBreak Forum
+      </a>
+      <ul className="flex items-center justify-end gap-9 text-[#3B3C4A] lg:flex-1 max-md:hidden dakr:text-[#FFFFFF]">
+        <li>Hi, Clinton</li>
+        <li>Logout</li>
+        <li>Login</li>
+        <li>Register</li>
+        <li className="font-semibold">Create Post</li>
+      </ul>
+
+      <Switch />
+      <FaHamburger className="text-2xl cursor-pointer hidden max-md:block dark:text-white" />
+    </nav>
+  );
+};
+
+export default NavBar;
