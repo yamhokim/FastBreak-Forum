66fcd9d6736a7215a66f45d4106e52fe7a47ad31
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-25 15:00:52 -0500

    implemented the blogcontainer component

---
+++ frontend/src/ui_components/BlogContainer.jsx
@@ -0,0 +1,23 @@
+import BlogCard from "./BlogCard";
+
+const BlogContainer = () => {
+  return (
+    <section className="padding-x py-6 max-container">
+      <h2 className="font-semibold text-xl mb-6 dark:text-white text-center">
+        📫 Latest Posts
+      </h2>
+
+      <div className="flex items-center gap-6 justify-center flex-wrap">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </div>
+    </section>
+  );
+};
+
+export default BlogContainer;
