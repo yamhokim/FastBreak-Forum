a537f40fe8d2a0bb3ed7c91ee4947174ca220c65
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-25 15:04:59 -0500

    added the blogcontainer component into the main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./ui_components/navbar";
 import Header from "./ui_components/Header";
+import BlogContainer from "./ui_components/BlogContainer";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <BlogContainer />
     </div>
   );
 };
