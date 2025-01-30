840afc2eab904a2664647f8f9f06a69f8bcf2463
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-29 21:42:56 -0500

    updated pagination component so that the previous and next buttons don't show for the first and last pages respectively

--- frontend/src/ui_components/PagePagination.jsx
+++ frontend/src/ui_components/PagePagination.jsx
@@ -10,13 +10,17 @@ import {
 
 const PagePagination = ({ numPages, handleSetPage, page }) => {
   const numbers = Array.from({ length: numPages }, (_, i) => i + 1);
+  const firstPageNum = numbers[0];
+  const lastPageNum = numbers[numbers.length - 1];
 
   return (
     <Pagination className="my-6 dark:text-white">
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
+        {page === firstPageNum || (
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+        )}
 
         {numbers.map((num) => (
           <PaginationItem key={num} onClick={() => handleSetPage(num)}>
@@ -30,9 +34,11 @@ const PagePagination = ({ numPages, handleSetPage, page }) => {
           </PaginationItem>
         ))}
 
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
+        {page === lastPageNum || (
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        )}
       </PaginationContent>
     </Pagination>
   );
