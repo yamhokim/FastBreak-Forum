aecc6322fbe2fd8fdf46acf15ba43225d687c343
yamhokim <yoonho.luke.kim@gmail.com>
2025-01-31 18:28:45 -0500

    used reat useForm to keep track of inputs for login credentials, as well as error handling

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,19 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <form
+      onSubmit={handleSubmit(onSubmit)}
       className="md:px-16 px-8 py-6 flex flex-col mx-auto my-9 items-center gap-4 w-fit rounded-lg bg-[#FFFFFF] shadow-xl 
     dark:text-white dark:bg-[#141624]"
     >
@@ -21,8 +30,12 @@ const LoginPage = () => {
           type="text"
           id="username"
           placeholder="Enter Username"
+          {...register("username", { required: "Username is requiered" })}
           className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
         />
+        {errors?.username?.message && (
+          <small className="text-red-700 ">{errors.username.message}</small>
+        )}
       </div>
 
       <div>
@@ -33,8 +46,12 @@ const LoginPage = () => {
           type="text"
           id="password"
           placeholder="Enter Password"
+          {...register("password", { required: "Password is requiered" })}
           className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
         />
+        {errors?.password?.message && (
+          <small className="text-red-700">{errors.password.message}</small>
+        )}
       </div>
 
       <div className="w-full flex items-center justify-center flex-col my-4">
